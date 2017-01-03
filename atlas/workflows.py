b3dcc1e417f39b6e744cc152c127a132c6004a51
Joe Brown <joe.brown@pnnl.gov>
2017-01-03 15:07:37 -0800

    fixes non-standard exit code

--- atlas/workflows.py
+++ atlas/workflows.py
@@ -10,8 +10,12 @@ def assemble(config, jobs, out_dir):
     snakefile = os.path.join(os.path.dirname(os.path.join(os.path.dirname(os.path.abspath(__file__)))), "Snakefile")
     if not os.path.exists(snakefile):
         sys.exit("Unable to locate the Snakemake workflow file; tried %s" % snakefile)
-    check_call(("snakemake -s {snakefile} -d {out_dir} -p -j {jobs} --configfile {config} "
-                "--nolock --config workflow=complete").format(snakefile=snakefile,
-                                                              out_dir=out_dir,
-                                                              jobs=jobs,
-                                                              config=config), shell=True)
+    try:
+        check_call(("snakemake -s {snakefile} -d {out_dir} -p -j {jobs} --configfile {config} "
+                    "--nolock --config workflow=complete").format(snakefile=snakefile,
+                                                                  out_dir=out_dir,
+                                                                  jobs=jobs,
+                                                                  config=config), shell=True)
+    except:
+        # the error will be printed in the snakemake log
+        pass
