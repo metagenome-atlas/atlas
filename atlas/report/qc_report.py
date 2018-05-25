dd6190e78c5e8ac449c2b899045b6666dcd1fb3f
kiesers <silas.kieser@gmail.com>
2018-05-25 16:57:08 +0200

    made qc_report work.

--- atlas/report/qc_report.py
+++ atlas/report/qc_report.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
 import zipfile
@@ -7,24 +8,19 @@ from plotly import offline, tools
 from cufflinks import iplot
 from snakemake.utils import report
 
+
 PLOTLY_PARAMS = dict(
     include_plotlyjs=False, show_link=False, output_type="div", image_height=700
 )
-read_counts = snakemake.input.read_counts
-report_out = snakemake.output.report
-zip_files_ = snakemake.input.zipfiles
-SAMPLES = snakemake.params.samples
-min_quality = snakemake.config["preprocess_minimum_base_quality"]
-
 
 def get_stats_from_zips(zips):
     # def get_read_stats(samples, step):
     quality_pe = pd.DataFrame()
-    quality_se = {}
+    quality_se = pd.DataFrame()
     for zfile in zips:
         zf = zipfile.ZipFile(zfile)
         # local testing files
-        sample = os.path.basename(zfile).partition("_")[0]
+        sample = zfile.split('/')[0]  # HACK: sample name is first name of path
         # relative path from snakemake
         # sample = zfile.partition(os.path.sep)[0]
         if "se/boxplot_quality.txt" in zf.namelist():
@@ -36,129 +32,175 @@ def get_stats_from_zips(zips):
                 df = pd.read_table(f, index_col=0)
                 df.columns = [df.columns, [sample] * df.shape[1]]
                 quality_pe = pd.concat((quality_pe, df[["mean_1", "mean_2"]]), axis=1)
+
     return quality_pe, quality_se
 
 
-def get_pe_read_quality_plot(df, **kwargs):
-    # div[variable] = offline.plot(
-    #     df[variable].iplot(
+def get_pe_read_quality_plot(df,quality_range, **kwargs):
+
+    #import pdb; pdb.set_trace()
+
+
+    fig = tools.make_subplots(rows=1, cols=2, shared_yaxes=True)
+
+    for sample in df["mean_1"].columns:
+        fig.append_trace({'x': df.index, 'y': df["mean_1"][sample].values,
+                          'type': 'scatter', 'name': sample}, 1, 1)
+
+    for sample in df["mean_2"].columns:
+        fig.append_trace({'x': df.index, 'y': df["mean_2"][sample].values,
+                          'type': 'scatter', 'name': sample}, 1, 2)
+
+
+    fig['layout'].update(
+                     yaxis = dict(range=quality_range, autorange=True, title="Quality score"),
+                     xaxis1 = dict(title='Position forward read'),
+                     xaxis2 = dict(autorange='reversed',title='Position reverse read')
+                     )
+
+
+    return offline.plot(fig, **kwargs, **PLOTLY_PARAMS)
+
+    #
+    # df1 = df[["mean_1", "mean_2"]]
+    # return offline.plot(
+    #     df1.iplot(
+    #         subplots=True,
+    #         shape=(1, 2),
+    #         shared_yaxes=True,
     #         asFigure=True,
-    #         kind="bar",
-    #         xTitle="Samples",
-    #         layout=go.Layout(
-    #             xaxis=dict(tickangle=45), yaxis=dict(title=y_axis_label)
-    #         ),
+    #         kind="line",
+    #         layout = go.Layout(
+    #             yaxis = dict(range=quality_range, autorange=True, title="Quality score"),
+    #             xaxis1 = dict(title='Position forward read'),
+    #             xaxis2 = dict(autorange='reversed',title='Position reverse read')
+    #                 )
     #     ),
-    #     **PLOTLY_PARAMS,
+    #
     # )
-    # cf.subplots([df1.figure(kind='heatmap'), df2.figure(kind='heatmap')]).iplot()
-    df1 = df[["mean_1", "mean_2"]]
+
+
+
+
+#,
+
+def draw_se_read_quality(df,quality_range,**kwargs):
     return offline.plot(
-        df1.iplot(
-            subplots=True,
-            shape=(1, 2),
-            shared_yaxes=True,
+        df.iplot(
             asFigure=True,
-            kind="bar",
-            xTitle="TODO",
-        ),
-        **kwargs,
-    )
+            kind="line",
+            layout = go.Layout(
+                yaxis = dict(range=quality_range, autorange=True, title="Quality score"),
+                xaxis = dict(title='Position read'),
+                            )
+                    ),**kwargs,**PLOTLY_PARAMS
+
+                    )
 
 
-def main():
+    # f, ax = plt.subplots(1, 1)
+    # Quality_se.plot(legend=False, ax=ax)
+    # ax.set_ylabel('Quality score')
+    # ax.set_xlabel('Postition')
+    # ax.set_ylim(quality_range)
+    # # again, kwargs should be added to this function
+    # return offline.plot_mpl(f, resize=True, **PLOTLY_PARAMS)
+    return ""
+
+def main(samples, report_out, read_counts, zipfiles_raw,zipfiles_QC, min_quality):
     div = {}
 
+    # N reads / N bases
+    df = pd.read_table(read_counts, index_col=[0, 1])
+    for variable in ['Total_Reads','Total_Bases']:
+        data = df[variable].unstack()[df.loc[df.index[0][0]].index]
+        div[variable] = offline.plot(
+                data.iplot(
+                    asFigure=True,
+                    kind='bar',
+                    xTitle='Samples',
+                    yTitle=variable.replace('_', ' '),
+                    layout=go.Layout(xaxis=dict(tickangle=45)),
+                ),
+                **PLOTLY_PARAMS,
+            )
 
-def draw_se_read_quality(Quality_se):
-    f, ax = plt.subplots(1, 1)
-    Quality_se.plot(legend=False, ax=ax)
-    ax.set_ylabel('Quality score')
-    ax.set_xlabel('Postition')
-    ax.set_ylim([min_quality, max_quality])
-    # again, kwargs should be added to this function
-    return offline.plot_mpl(f, resize=True, **plotly_params)
+
+    Report_numbers = """
+
+### Total reads per sample
 
 
-# N reads / N bases
-df = pd.read_table(read_counts, index_col=[0, 1])
-for variable in df.columns[:2]:
-    data = df[variable].unstack()[df.loc[df.index[0][0]].index]
-    div[variable] = plotly_embed_div(
-        offline.plot(
-            data.iplot(
-                asFigure=True,
-                kind='bar',
-                xTitle='Samples',
-                yTitle=variable.replace('_', ' '),
-                layout=layout_x_45,
-            ),
-            **plotly_params,
-        )
-    )
-Report_numbers = """
-    **********************
-    Total reads per sample
-    **********************
+.. raw:: html
 
     {div[Total_Reads]}
 
-    ============   ===================================
-    Step           Output
-    ============   ===================================
-    raw            the input reads
-    deduplicated   after (optional) deduplication step
-    filtered       trimmed, PhiX filtered
-    clean          contaminants removed
-    qc             passing reads (metag includes SSU)
-    ============   ===================================
+============   ===================================
+Step           Output
+============   ===================================
+raw            the input reads
+deduplicated   after (optional) deduplication step
+filtered       trimmed, PhiX filtered
+clean          contaminants removed
+qc             passing reads (metag includes SSU)
+============   ===================================
 
-    Total bases per sample
-    ----------------------
+### Total bases per sample
+
+.. raw:: html
 
     {div[Total_Bases]}
 
-    For details see Table T1_.
-    """
-report_params['T1'] = read_counts
-Report_read_quality_qc = """
-    **********************
-    Reads quality after QC
-    **********************
-    """
-Quality_pe, Quality_se = get_read_stats(SAMPLES, 'QC')
-max_quality = 1 + np.nanmax((Quality_pe.max().max(), Quality_se.max().max()))
-if Quality_pe.shape[0] > 0:
-    div['quality_qc_pe'] = plotly_embed_div(draw_pe_read_quality(Quality_pe))
-    Report_read_quality_qc += """
-    Paired end
-    ----------
+For details see Table Table1_.
+"""
+
+    Report_read_quality_qc = """
+
+### Reads quality after QC
+
+"""
+
+    Quality_pe, Quality_se = get_stats_from_zips(zipfiles_QC)
+    max_quality = 1 + np.nanmax((Quality_pe.max().max(), Quality_se.max().max()))
+    if Quality_pe.shape[0] > 0:
+        div['quality_qc_pe'] = get_pe_read_quality_plot(Quality_pe,[min_quality,max_quality])
+        Report_read_quality_qc += """
+#### Paired end
+
+.. raw:: html
 
     {div[quality_qc_pe]}
 
-    Single end
-    ----------
+#### Single end
 
-    Paired end reads that lost their mate during filtering.
 
-    """
-div['quality_qc_se'] = plotly_embed_div(draw_se_read_quality(Quality_se))
-Report_read_quality_qc += "\n    {div[quality_qc_se]}\n"
-Report_read_quality_raw = """
-    ***********************
-    Reads quality before QC
-    ***********************
+Paired end reads that lost their mate during filtering.
+
+"""
+    div['quality_qc_se'] = draw_se_read_quality(Quality_se,[min_quality,max_quality])
+    Report_read_quality_qc += """
+.. raw:: html
+
+    {div[quality_qc_se]}
+
+"""
+    Report_read_quality_raw = """
+
+### Reads quality before QC
+
+
+.. raw:: html
 
     {div[quality_raw]}
 
-    """
-Quality_pe, Quality_se = get_read_stats(SAMPLES, 'raw')
-if Quality_pe.shape[0] > 0:
-    div['quality_raw'] = plotly_embed_div(draw_pe_read_quality(Quality_pe))
-elif Quality_se.shape[0] > 0:
-    div['quality_raw'] = plotly_embed_div(draw_se_read_quality(Quality_se))
-else:
-    raise IndexError()
+"""
+    Quality_pe, Quality_se = get_stats_from_zips(zipfiles_raw)
+    if Quality_pe.shape[0] > 0:
+        div['quality_raw'] = get_pe_read_quality_plot(Quality_pe,[min_quality,max_quality])
+    elif Quality_se.shape[0] > 0:
+        div['quality_raw'] = draw_se_read_quality(Quality_se,[min_quality,max_quality])
+    else:
+        raise IndexError()
 
     report_str = """
 
@@ -181,15 +223,21 @@ Summary
 -------
 
 
+"""+\
+    Report_numbers+\
+    Report_read_quality_qc+\
+    Report_read_quality_raw+"""
 
 Downloads
 ---------
 
 """
+
+
     report(
         report_str,
         report_out,
-        Table_1=combined_stats,
+        Table1=read_counts,
         stylesheet=os.path.join(
             os.path.abspath(os.path.dirname(__file__)), "report.css"
         ),
@@ -199,13 +247,11 @@ Downloads
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("--samples", nargs="+")
-    ...
+    p.add_argument("--report_out")
+    p.add_argument("--read_counts")
+    p.add_argument("--zipfiles_raw", nargs="+")
+    p.add_argument("--zipfiles_QC", nargs="+")
+    p.add_argument("--min_quality")
     args = p.parse_args()
-    main(
-        args.samples,
-        args.contig_stats,
-        args.gene_tables,
-        args.mapping_logs,
-        args.report_out,
-        args.combined_stats,
-    )
+
+    main(**vars(args))
