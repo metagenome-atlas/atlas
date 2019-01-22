b8e1af3c7b7bd308d3760a2cabf5b7f0a34d4a14
kiesers <silas.kieser@gmail.com>
2019-01-22 15:04:59 +0100

    add new test

---
+++ test.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+set -euo pipefail
+
+
+#rm -rf WD
+
+
+
+atlas --version
+atlas run --help
+
+
+
+databaseDir="databases"
+
+rm -f WD/samples.tsv
+#
+atlas init --db-dir $databaseDir --threads 3 --assembler spades -w WD data_debug
+
+for w in qc assembly genomes genecatalog ; do
+
+  echo "
+        Dryrun Workflow $w
+      "
+
+  atlas run -w WD $w --dryrun $@
+
+done
+#
+atlas run -w WD --dryrun $@
+#
+atlas run -w WD qc $@
+
+
+
+#mkdir -p WD2/qcreads
+#cp WD/*/sequence_quality_control/*_QC_R?.fastq.gz WD2/qcreads
+
+#rm -f WD2/samples.tsv
+
+#atlas init --db-dir $databaseDir --threads 3 --assembler spades --skip-qc -w WD2 WD2/qcreads
+
+
+atlas run -w WD --workflow assembly $@
