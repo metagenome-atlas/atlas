34075c4c48bc405540ff4fe077ee79a8f8485514
kiesers <silas.kieser@gmail.com>
2019-03-18 16:25:27 +0100

    add test_binning

---
+++ .test/test_binning.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+set -euo pipefail
+
+
+
+
+
+atlas --version
+
+
+
+
+databaseDir=".test/databases"
+WD='.test/Test_binning'
+reads_dir=".test/reads/binning"
+
+ressource_args=" --config java_mem=10 assembly_mem=10"
+
+
+# gen randomreads
+snakemake -s atlas/rules/testing.smk -d $reads_dir --config reads=50000
+
+
+rm -f $WD/samples.tsv
+#
+atlas init --db-dir $databaseDir --threads 3  -w $WD $reads_dir
+
+atlas run binning -w $WD $ressource_args assembler=spades $@
