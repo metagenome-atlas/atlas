0a28a71d326054d36dc87e5f1d922ce55b0c1a6f
kiesers <silas.kieser@gmail.com>
2019-03-18 18:02:25 +0100

    need databases for run all
    get only genomes/genomes

--- .test/test_binning.sh
+++ .test/test_binning.sh
@@ -27,6 +27,7 @@ atlas init --db-dir $databaseDir --threads 3  -w $WD $reads_dir
 
 atlas run binning -w $WD $ressource_args assembler=spades final_binner=metabat $@
 
-atlas run genomes -w $WD $ressource_args assembler=spades final_binner=metabat $@
+# genomes need databases
+atlas run None -w $WD $ressource_args assembler=spades final_binner=metabat genomes/genomes $@
 
-atlas run all -w $WD $ressource_args assembler=spades final_binner=metabat $@
+#atlas run all -w $WD $ressource_args assembler=spades final_binner=metabat $@
