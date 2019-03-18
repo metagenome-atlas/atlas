0114b5b75d0f2a9a5b677a74ee7696eaee015a5d
kiesers <silas.kieser@gmail.com>
2019-03-18 17:58:12 +0100

    use example data for dryrun

--- .test/dryrun.sh
+++ .test/dryrun.sh
@@ -13,23 +13,21 @@ atlas run --help
 
 databaseDir=".test/databases"
 WD='.test/Dryrun'
-reads_dir=".test/reads/stub"
+reads_dir=example_data
 
 rm -fr $WD
 #
 # gen randomreads
 #very low number only for assembly
-#snakemake -s atlas/rules/testing.smk -d $reads_dir --config reads=1000
+snakemake -s atlas/rules/testing.smk -d $reads_dir --config reads=1000
 
 
-
-atlas init --db-dir $databaseDir --threads 3 -w $WD $reads_dir
 
 
 atlas download --db-dir $databaseDir -n
 
 
-atlas init --db-dir $databaseDir --threads 3 -w $WD --assembler spades example_data
+atlas init --db-dir $databaseDir --threads 3 -w $WD --assembler spades $reads_dir
 
 
 for w in qc assembly genomes genecatalog ; do
@@ -49,7 +47,7 @@ atlas run -w $WD --dryrun $@
 
 WD=${WD}_noQC
 rm -fr $WD
-atlas init --db-dir $databaseDir --threads 3 --skip-qc -w $WD --assembler megahit example_data
+atlas init --db-dir $databaseDir --threads 3 --skip-qc -w $WD --assembler megahit $reads_dir
 
 
 for w in assembly genomes genecatalog ; do
