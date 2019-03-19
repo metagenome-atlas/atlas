d00788954cc37548396a896792537a9d6e3508f1
kiesers <silas.kieser@gmail.com>
2019-03-19 13:15:16 +0100

    simplify dryrun

--- .test/dryrun.sh
+++ .test/dryrun.sh
@@ -16,12 +16,6 @@ WD='.test/Dryrun'
 reads_dir=example_data
 
 rm -fr $WD
-#
-# gen randomreads
-#very low number only for assembly
-snakemake -s atlas/rules/testing.smk -d $reads_dir --config reads=1000
-
-
 
 
 atlas download --db-dir $databaseDir -n
@@ -30,34 +24,24 @@ atlas download --db-dir $databaseDir -n
 atlas init --db-dir $databaseDir --threads 3 -w $WD --assembler spades $reads_dir
 
 
-for w in qc assembly genomes genecatalog ; do
-
-  echo "
-        Dryrun Workflow $w
-      "
-
-  atlas run $w -w $WD --dryrun $@
-
-done
+# for w in qc assembly genomes genecatalog ; do
+#
+#   echo "
+#         Dryrun Workflow $w
+#       "
+#
+#   atlas run $w -w $WD --dryrun $@
+#
+# done
 #
 atlas run -w $WD --dryrun $@
 #
 
-# skip QC
-
-WD=${WD}_noQC
-rm -fr $WD
-atlas init --db-dir $databaseDir --threads 3 --skip-qc -w $WD --assembler megahit $reads_dir
-
-
-for w in assembly genomes genecatalog ; do
-
-  echo "
-        Dryrun Workflow $w
-      "
-
-  atlas run $w -w $WD --dryrun $@
-
-done
+# # skip QC
 #
-atlas run -w $WD --dryrun $@
+# WD=${WD}/noQC
+# rm -fr $WD
+# atlas init --db-dir $databaseDir --threads 3 --skip-qc -w $WD --assembler megahit $reads_dir
+#
+#
+# atlas run all -w $WD --dryrun $@
