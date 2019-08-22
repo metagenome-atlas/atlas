f703f41bd5b18afbd9d1a44f7756ac13495f2115
kiesers <silas.kieser@gmail.com>
2019-08-22 21:12:36 +0200

    doen't test for additonal sample headers

--- atlas/conf.py
+++ atlas/conf.py
@@ -60,7 +60,7 @@ def get_samples_from_fastq(path):
 def validate_sample_table(sampleTable):
 
 
-    Expected_Headers =['BinGroup'] + ADDITIONAL_SAMPLEFILE_HEADERS
+    Expected_Headers =['BinGroup'] #+ ADDITIONAL_SAMPLEFILE_HEADERS
     for h in Expected_Headers:
         if not (h in sampleTable.columns):
          logging.error(f"expect '{h}' to be found in samples.tsv")
