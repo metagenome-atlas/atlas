98aa508e36c4dc133b4c4eccf757dd22e9492333
kiesers <silas.kieser@gmail.com>
2019-12-11 15:16:36 +0100

    build tree for genomes

--- .test/test_binning.sh
+++ .test/test_binning.sh
@@ -17,6 +17,5 @@ atlas init --db-dir $databaseDir --threads 4  -w $WD --skip-qc $reads_dir
 atlas run None "reports/bin_report_DASTool.html" --config interleaved_fastqs=True -w $WD $@
 
 # genomes need databases
-atlas run None "genomes/clustering/allbins2genome.tsv" "genomes/counts/raw_counts_genomes.tsv" --config interleaved_fastqs=True -w $WD $@
-
-#atlas run all -w $WD $ressource_args assembler=spades final_binner=metabat $@
+genome_files="genomes/clustering/allbins2genome.tsv genomes/counts/raw_counts_genomes.tsv genomes/tree/checkm.nwk"
+atlas run None $genome_files --config interleaved_fastqs=True -w $WD $@
