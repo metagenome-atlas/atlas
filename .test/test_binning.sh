19d7bf8166f85e50bc12921ba2463de56a5b68bb
kiesers <silas.kieser@gmail.com>
2019-12-13 09:41:20 +0100

    import checkm data for testing

--- .test/test_binning.sh
+++ .test/test_binning.sh
@@ -6,7 +6,7 @@ atlas --version
 databaseDir="databases"
 WD='example_data/binning'
 reads_dir="example_data/reads/test"
-config="--config interleaved_fastqs=True final_binner=maxbin threads=2 mem=4 java_mem=4"
+config="--config interleaved_fastqs=True final_binner=maxbin threads=2 mem=4 java_mem=4 "
 
 rm -f $WD/samples.tsv $WD/finished_assembly
 touch $WD/finished_assembly
@@ -14,8 +14,17 @@ touch $WD/finished_assembly
 #
 atlas init --db-dir $databaseDir --threads 4  -w $WD --skip-qc $reads_dir
 
-atlas run None "reports/bin_report_metabat.html" $config -w $WD $@
+atlas run None binning $config -w $WD $@ --omit-from get_bins
+
+echo "Copy checkm files"
+
+for s in "sample1 sample2" ;
+do
+  mkdir -p $WD/$s/binning/DASTool/checkm
+  mv example_data/binning/checkm_results/$s/* $WD/$s/binning/DASTool/checkm
+done
+
+atlas run None binning $config -w $WD $@
 
 # genomes need databases
-genome_files="genomes/clustering/allbins2genome.tsv genomes/counts/raw_counts_genomes.tsv genomes/tree/checkm.nwk"
-atlas run None $genome_files $config -w $WD $@
+atlas run None genomes/clustering/allbins2genome.tsv  $config -w $WD $@
