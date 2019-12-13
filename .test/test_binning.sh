f68376dafcbc236e56454037a941c2a43b91f65a
kiesers <silas.kieser@gmail.com>
2019-12-13 10:09:15 +0100

    test with interleaved reads

--- .test/test_binning.sh
+++ .test/test_binning.sh
@@ -6,13 +6,13 @@ atlas --version
 databaseDir="databases"
 WD='example_data/binning'
 reads_dir="example_data/reads/test"
-config="--config interleaved_fastqs=True final_binner=maxbin threads=2 mem=4 java_mem=4"
+config="--config final_binner=maxbin threads=2 mem=4 java_mem=4"
 
 rm -f $WD/samples.tsv $WD/finished_assembly
 touch $WD/finished_assembly
 
 #
-atlas init --db-dir $databaseDir --threads 4  -w $WD --skip-qc $reads_dir
+atlas init --db-dir $databaseDir --threads 4  -w $WD --skip-qc --interleaved-fastq $reads_dir
 
 atlas run None "reports/bin_report_metabat.html" $config -w $WD $@
 
