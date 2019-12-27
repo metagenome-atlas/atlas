728f536909f3a201e4173caa5bd9d8ad74cf9cda
kiesers <silas.kieser@gmail.com>
2019-12-27 23:42:00 +0100

    update test_atlas.sh

--- test_atlas.sh
+++ test_atlas.sh
@@ -7,18 +7,17 @@ databases=./databases # path to database dir where you can store some 100gb data
 # install atlas
 git clone https://github.com/metagenome-atlas/atlas.git
 cd atlas
-conda create f atlasenv.yml
+conda env create -n atlasenv -f atlasenv.yml
 conda activate atlasenv
 pip install --editable .
 
 
 # set up cluster integration
 mkdir -p ~/.config/snakemake
-cd ~/.config/snakemake
-cookiecutter https://github.com/metagenome-atlas/clusterprofile.git
+cookiecutter https://github.com/metagenome-atlas/clusterprofile.git -o ~/.config/snakemake
 
 cd $curdir
 #test run
 git clone https://github.com/metagenome-atlas/example_data.git
-atlas init --db-dir $databases --working-dir testrun example_data/reads/test
+atlas init --db-dir $databases --working-dir testrun --interleaved-fastq example_data/reads/test
 atlas run all --working-dir testrun --profile cluster
