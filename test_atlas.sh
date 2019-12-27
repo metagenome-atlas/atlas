81648cb72b70510842a1952931e4783d18642eb2
kiesers <silas.kieser@gmail.com>
2019-12-27 23:44:07 +0100

    add dryrun before

--- test_atlas.sh
+++ test_atlas.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-curdir=$(pwd)
-
 databases=./databases # path to database dir where you can store some 100gb databases
 
 # install atlas
@@ -11,13 +9,15 @@ conda env create -n atlasenv -f atlasenv.yml
 conda activate atlasenv
 pip install --editable .
 
+atlas --help
+
+#test run
+git clone https://github.com/metagenome-atlas/example_data.git
+atlas init --db-dir $databases --working-dir testrun --interleaved-fastq example_data/reads/test
 
 # set up cluster integration
 mkdir -p ~/.config/snakemake
 cookiecutter https://github.com/metagenome-atlas/clusterprofile.git -o ~/.config/snakemake
 
-cd $curdir
-#test run
-git clone https://github.com/metagenome-atlas/example_data.git
-atlas init --db-dir $databases --working-dir testrun --interleaved-fastq example_data/reads/test
+atlas run all --working-dir testrun --profile cluster --dryrun
 atlas run all --working-dir testrun --profile cluster
