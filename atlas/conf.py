f782de53776f89d35d79cd4ca92f9531a20134c5
Silas Kieser <silas.kieser@gmail.com>
2021-01-19 20:17:58 +0100

    bug

--- atlas/conf.py
+++ atlas/conf.py
@@ -328,12 +328,12 @@ def run_init(path_to_fastq,db_dir, working_dir, assembler,  data_type, interleav
 
 @click.command(
     "init-public",
-    short_help= "Prepare atlas run from public data from SRA"
+    short_help= "Prepare atlas run from public data from SRA",
     help="prepare configuration file and sample table for atlas run"
                 "based on public data from SRA\n"
                 "Supply a set of SRA run ids to the command:"
                 "SRR4305427 ERR1190946\n\n"
-                "Reads are automatically downloaded but not stored on your machine. Beware that sometimes multiple runs go into one sample.",
+                "Reads are automatically downloaded but not stored on your machine. Beware that sometimes multiple runs go into one sample."
 )
 @click.argument("identifiers",nargs=-1)
 @click.option(
