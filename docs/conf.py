c8a8629974f337f0cc7dbadb8bb879c6d5dedf63
silask <silas.kieser@gmail.com>
2021-10-29 16:24:29 +0200

    docs: book theme

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
