a24428c7c5d3792be2d445ece8a36c602d2d5c9a
Silas Kieser <SilasK@users.noreply.github.com>
2022-11-09 09:11:54 +0100

    use minimap for genomes (#569)
    * let's hope this works
    
    * minimap
    
    * remove bbyplit
    
    * filter genomes my self
    
    * formating
    
    * formating
    
    * undo formating
    
    * rename checkm stats instead of rerunning on MAGs
    
    * formating
    
    * remove bbsplit
    
    * put genome quantification in genomes.smk
    
    * typo
    
    * pileup on orfs
    
    * better combine genome coverage
    
    * fix merging

--- atlas/workflow/scripts/utils/parsers_checkm.py
+++ atlas/workflow/scripts/utils/parsers_checkm.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from warnings import warn
 
 
 def read_checkm_output(taxonomy_table, completness_table):
@@ -21,3 +22,30 @@ def read_checkm_output(taxonomy_table, completness_table):
     ]
     df = pd.concat([c_df, t_df], axis=1)
     return df
+
+
+def load_quality(quality_file):
+    Q = pd.read_csv(quality_file, index_col=0, sep="\t")
+
+    # remove extension if present
+    if Q.index.str.contains(".fa").all():
+        warn("Found fasta extension in index. I remove them")
+        Q.index = Q.index.str.split(".fa", expand=True).to_frame()[0]
+
+    Q.columns = Q.columns.str.lower()
+
+    necessary_columns = ["Completeness", "Contamination"]
+
+    # rename lower and uppercase to necessary_columns
+    Q = Q.rename(
+        columns={fun(s): s for s in necessary_columns for fun in (str.lower, str.upper)}
+    )
+
+    if Q.columns.isin(necessary_columns).sum() != len(necessary_columns):
+        raise Exception(
+            f"{necessary_columns} should be in the quality table, only got {Q.columns}"
+        )
+
+    assert not Q.index.duplicated().any(), f"duplicated indexes in {quality_file}"
+
+    return Q
