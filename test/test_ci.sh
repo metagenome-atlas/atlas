8f01959e069ae52156c8b5572ac5fce4d369c2c6
Silas Kieser <silas.kieser@gmail.com>
2023-05-03 15:53:25 +0200

    test_ci all

---
+++ test/test_ci.sh
@@ -0,0 +1,48 @@
+#! /usr/bin/env bash
+set -euox pipefail
+
+
+
+
+
+atlas --version
+
+
+
+
+databaseDir="databases"
+WD='test/Test_assembly'
+reads_dir="example_data/reads/test"
+
+
+rm -f $WD/samples.tsv
+#
+atlas init --db-dir $databaseDir  -w $WD $reads_dir
+
+atlas run -w $WD qc  $@
+
+echo "\n\nFinished qc\n\n"
+
+
+atlas run assembly -w $WD $@
+
+echo "\n\nFinished assembly\n\n"
+
+atlas run binning -w $WD $@
+
+echo "\n\nFinished binning\n\n"
+
+atlas run genecatalog --omit-from combine_egg_nogg_annotations combine_dram_genecatalog_annotations -w $WD $@
+
+echo "\n\nFinished genecatalog\n\n"
+
+atlas run genomes -w $WD $@
+
+echo "\n\nFinished genomes\n\n"
+
+atlas run all -w $WD $@
+
+echo "\n\nFinished all\n\n"
+
+
+
